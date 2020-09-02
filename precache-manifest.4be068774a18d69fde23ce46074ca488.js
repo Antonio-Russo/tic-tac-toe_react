b6b4bcb429446282f0486673606031c1ab56aacc
Giovanna Di Meglio <reactivegio@gmail.com>
2020-09-02 12:41:22 +0200

    Updates

---
+++ precache-manifest.4be068774a18d69fde23ce46074ca488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a75454c5a6b3c77e62f96526e8fab45",
+    "url": "/tic-tac-toe_react/index.html"
+  },
+  {
+    "revision": "f3f139f2fc1af605fa01",
+    "url": "/tic-tac-toe_react/static/css/main.e4b5b421.chunk.css"
+  },
+  {
+    "revision": "32fb53c22775826a48af",
+    "url": "/tic-tac-toe_react/static/js/2.c2d91ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe_react/static/js/2.c2d91ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3f139f2fc1af605fa01",
+    "url": "/tic-tac-toe_react/static/js/main.4fb45807.chunk.js"
+  },
+  {
+    "revision": "0b64b07186b51ee3aecd",
+    "url": "/tic-tac-toe_react/static/js/runtime-main.956ef354.js"
+  }
+]);
